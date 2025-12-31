0651b62006f485df5b6e226a4f7364e7824a835a
Marin Weis <weis.e2400483@etud.univ-ubs.fr>
2025-12-31 11:08:31 +0100

    Mise à jour de la version de la base de données (1 -> 2) dans AppDatabase.kt

--- app/src/main/java/fr/iutvannes/dual/model/database/AppDatabase.kt
+++ app/src/main/java/fr/iutvannes/dual/model/database/AppDatabase.kt
@@ -15,7 +15,7 @@ import fr.iutvannes.dual.model.persistence.Classe
 
 @Database(
     entities = [Eleve::class, Prof::class, Resultat::class, Seance::class, Classe::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
