154584d5ec5bfc1859d558b21c6004728f74bbcc
Matthieu <matthieugouelo@gmail.com>
2026-03-03 14:12:08 +0100

    Modification des tables Tir et Course + DAO

--- app/src/main/java/fr/iutvannes/dual/model/database/AppDatabase.kt
+++ app/src/main/java/fr/iutvannes/dual/model/database/AppDatabase.kt
@@ -3,22 +3,22 @@ package fr.iutvannes.dual.model.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import fr.iutvannes.dual.model.persistence.Classe
-import fr.iutvannes.dual.model.persistence.Courses
+import fr.iutvannes.dual.model.persistence.Course
 import fr.iutvannes.dual.model.persistence.Eleve
 import fr.iutvannes.dual.model.persistence.Prof
 import fr.iutvannes.dual.model.persistence.Resultat
 import fr.iutvannes.dual.model.persistence.Seance
-import fr.iutvannes.dual.model.persistence.Tirs
+import fr.iutvannes.dual.model.persistence.Tir
 import fr.iutvannes.dual.model.dao.ClasseDAO
-import fr.iutvannes.dual.model.dao.CoursesDAO
+import fr.iutvannes.dual.model.dao.CourseDAO
 import fr.iutvannes.dual.model.dao.EleveDAO
 import fr.iutvannes.dual.model.dao.ProfDAO
 import fr.iutvannes.dual.model.dao.ResultatDAO
 import fr.iutvannes.dual.model.dao.SeanceDAO
-import fr.iutvannes.dual.model.dao.TirsDAO
+import fr.iutvannes.dual.model.dao.TirDAO
 
 @Database(
-    entities = [Eleve::class, Prof::class, Resultat::class, Seance::class, Classe::class, Tirs::class, Courses::class],
+    entities = [Eleve::class, Prof::class, Resultat::class, Seance::class, Classe::class, Tir::class, Course::class],
     version = 2,
     exportSchema = false
 )
@@ -29,6 +29,6 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun resultatDao(): ResultatDAO
     abstract fun seanceDao(): SeanceDAO
     abstract fun classeDao(): ClasseDAO
-    abstract fun tirsDao(): TirsDAO
-    abstract fun coursesDao(): CoursesDAO
+    abstract fun tirDao(): TirDAO
+    abstract fun courseDao(): CourseDAO
 }
